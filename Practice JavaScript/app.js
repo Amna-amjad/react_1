7a737858ff7b56a9d281a634df8ad81bcbb72f2d
Amna_amjad <amnakhan8685@gmail.com>
2021-09-18 10:32:26 +0500

    commit

---
+++ Practice JavaScript/app.js
@@ -0,0 +1,2 @@
+let myVar = String(34);
+console.log(myVar, (typeof myVar));
---
+++ prototypes/app.js
@@ -0,0 +1,74 @@
+// Conctructor Functions
+// function Std(name, id, gender){
+//     this.name = name
+//     this.id = id
+//     this.gender = gender
+// }
+// var std1 = new Std("Amna", 1 , "female")
+// var std2 = new Std("Amna1", 2 , "female")
+// console.log(std1);
+
+
+
+
+// Methods
+
+// var obj = {
+//     name : "Amna",
+//     getName : function(){
+//      return this.name
+//     }
+// }
+// console.log(obj.getName());
+
+// Prototypes
+ 
+// function Std(firstName, lastName){
+//     this.fname = firstName
+//     this.lname = lastName
+// }
+// Std.prototype.getFullName = function(){
+
+//     return this.fname + " " + this.lname 
+
+// }
+// var std1 = new Std("Amna", "Khan")
+// console.log(std1.getFullName());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
