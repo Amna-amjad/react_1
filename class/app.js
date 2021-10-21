1bc3b775e9aebb2f2d3b02e3b7cc64557bfa55eb
Amna_amjad <amnakhan8685@gmail.com>
2021-10-21 10:55:43 +0500

    msg

--- class/app.js
+++
@@ -1,8 +0,0 @@
-var age = prompt("Enter your age");
-// if(age >= 18 && age <= 40){
-//  alert("You are eligible")
-// }
-// else{
-//     alert("You are not eligible");
-// }
-if 
---
+++ react_props/react_props/src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import MediaCard from './components/MediaCard';
+
+function App() {
+  return (
+   <MediaCard title = "Black cars are Love" body = "This is the latest edition of cars" img="https://wallpaperforu.com/wp-content/uploads/2020/07/car-wallpaper-200726122352281920x1200.jpg "/> 
+  );
+}
+
+export default App;
